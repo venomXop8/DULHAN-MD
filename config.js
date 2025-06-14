95c1c53f85c99acda2044fb5e255cddbaff16c95
Ata Ur Rehman Malik <119836227+arkhan998@users.noreply.github.com>
2025-06-14 17:55:10 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+// DULHAN-MD Configuration File
+
+// Yeh file ab aapki details ke saath mukammal hai.
+
+module.exports = {
+  SESSION_ID: "", // Isko khaali chhod dein kyunki hum QR code istemal kar rahe hain
+  
+  BOT_NAME: "DULHAN-MD",
+  
+  OWNER_NAME: "Malik Sahab", // Aapka naam
+  
+  OWNER_NUMBER: "923322964709", // Aapka number
+  
+  CHANNEL_LINK: "https://whatsapp.com/channel/0029VaN8WMOHFxP0SLAKKu0P", // Aapka channel link
+  
+  AUDIO_REPLY_PATH: "./dulhan_audio.mp3",
+  
+  PROFILE_PIC_URL: "https://files.catbox.moe/wz96cv.jpg",
+  
+  PREFIX: ".",
+};
