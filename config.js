786f3b906c2e850cca683e29553143ce4e08f6a7
Ata Ur Rehman Malik <119836227+arkhan998@users.noreply.github.com>
2025-06-15 10:57:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,22 @@
 // DULHAN-MD Configuration File
 
 module.exports = {
-  SESSION_ID: "", // Isko khaali chhod dein
+  // ▼▼▼ YAHAN APNI LAMBI WALI SESSION ID PASTE KAREIN ▼▼▼
+  SESSION_ID: "DULHAN-MD~qeyerekese", 
   
-  // ▼▼▼ YAHAN APNI GEMINI API KEY DAALEIN ▼▼▼
   GEMINI_API_KEY: "AIzaSyDDPoDW5-4wOz2xUPtX0LF2HXvTUxN8VAY", 
   
   BOT_NAME: "DULHAN-MD",
+  
   OWNER_NAME: "Malik Sahab",
+  
   OWNER_NUMBER: "923322964709",
+  
   CHANNEL_LINK: "https://whatsapp.com/channel/0029VaN8WMOHFxP0SLAKKu0P",
+  
   AUDIO_REPLY_PATH: "./dulhan_audio.mp3",
+  
   PROFILE_PIC_URL: "https://files.catbox.moe/wz96cv.jpg",
+  
   PREFIX: ".",
 };
