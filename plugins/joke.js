ff75c241df7a137cd628fde056996b56412ebbf6
Ata Ur Rehman Malik <119836227+arkhan998@users.noreply.github.com>
2025-06-19 09:24:44 +0500

    Update joke.js

--- plugins/joke.js
+++ plugins/joke.js
@@ -1,27 +1,16 @@
-// plugins/joke.js
 const axios = require('axios');
 module.exports = {
   command: ['joke', 'lateefa'],
   description: 'Tells a random joke.',
   category: 'fun',
   async handler(m) {
+    const { reply } = m;
     try {
-        const { data } = await axios.get('https://v2.jokeapi.dev/joke/Any?type=single');
-        m.reply(`*Here's a joke for you:*\n\n${data.joke}`);
-    } catch (e) { m.reply('Sorry, abhi koi joke yaad nahi aa raha.'); }
-  }
-};
-```javascript
-// plugins/meme.js
-const axios = require('axios');
-module.exports = {
-  command: ['meme'],
-  description: 'Sends a random meme.',
-  category: 'fun',
-  async handler(m) {
-    try {
-        const { data } = await axios.get('[https://meme-api.com/gimme](https://meme-api.com/gimme)');
-        await m.sock.sendMessage(m.key.remoteJid, { image: { url: data.url }, caption: `*${data.title}*`}, { quoted: m });
-    } catch (e) { m.reply('Sorry, memes ka server down hai.'); }
+        const { data } = await axios.get('https://v2.jokeapi.dev/joke/Any?type=single&safe-mode');
+        reply(`*Here's a joke for you:*\n\n${data.joke}`);
+    } catch (e) {
+        console.error("Joke API Error:", e);
+        reply('Sorry, abhi koi joke yaad nahi aa raha.');
+    }
   }
 };
