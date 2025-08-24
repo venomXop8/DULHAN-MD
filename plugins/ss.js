db632003484c38f53bc0ee73ba6e6059dd4eb778
Ata Ur Rehman Malik <119836227+arkhan998@users.noreply.github.com>
2025-08-24 16:33:01 +0500

    Create ss.js

---
+++ plugins/ss.js
@@ -0,0 +1,16 @@
+//  (Website Screenshot)
+const axios = require('axios');
+module.exports = {
+    command: ['ss', 'screenshot'],
+    description: 'Takes a screenshot of a website.',
+    category: 'utility',
+    async handler(m, { text }) {
+        if (!text || !text.startsWith('http')) return m.reply('Please provide a valid website URL.');
+        try {
+            await m.reply(`Taking screenshot of ${text}...`);
+            // Note: Get your free access key from screenshotone.com and replace YOUR_ACCESS_KEY
+            const url = `https://api.screenshotone.com/take?access_key=YOUR_ACCESS_KEY&url=${encodeURIComponent(text)}&full_page=true`;
+            await m.sock.sendMessage(m.key.remoteJid, { image: { url: url }, caption: `Screenshot of ${text}` }, { quoted: m });
+        } catch (e) { m.reply('Sorry, could not take a screenshot.'); }
+    }
+};
