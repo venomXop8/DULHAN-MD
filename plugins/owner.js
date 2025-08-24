d939adbff293e28f0415d01d7348740f6f9b6dab
Ata Ur Rehman Malik <119836227+arkhan998@users.noreply.github.com>
2025-08-24 15:46:10 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,30 +1,23 @@
+const fs = require('fs');
+
 module.exports = {
   command: ['owner', 'creator'],
   description: 'Shows the contact of the bot owner.',
   category: 'main',
   async handler(m, { sock, config }) {
     const ownerVCard = `BEGIN:VCARD\n`
-                     + `VERSION:3.0\n` 
-                     + `FN:${config.OWNER_NAME}\n` 
+                     + `VERSION:3.0\n`
+                     + `FN:${config.OWNER_NAME}\n`
                      + `ORG:${config.BOT_NAME};\n`
                      + `TEL;type=CELL;type=VOICE;waid=${config.OWNER_NUMBER}:${config.OWNER_NUMBER}\n`
                      + `END:VCARD`;
 
     await sock.sendMessage(
         m.key.remoteJid,
-        { 
-            contacts: { 
-                displayName: config.OWNER_NAME, 
-                contacts: [{ vcard: ownerVCard }] 
-            },
-            contextInfo: { // Yahan bhi channel ka zikr kar dete hain
-                externalAdReply: {
-                    title: `👰‍♀️ ${config.BOT_NAME}`,
-                    body: `Owner: ${config.OWNER_NAME}`,
-                    thumbnail: fs.readFileSync('./dulhan_thumbnail.jpg'),
-                    sourceUrl: config.CHANNEL_LINK,
-                    mediaUrl: config.CHANNEL_LINK,
-                }
+        {
+            contacts: {
+                displayName: config.OWNER_NAME,
+                contacts: [{ vcard: ownerVCard }]
             }
         },
         { quoted: m }
