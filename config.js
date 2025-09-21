53666db72ece70dcc57ba4c1de46744f1207da7c
IRONMAN x VENOM <gautamdipanshu475@gmail.com>
2025-09-22 01:33:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // DULHAN-MD Configuration File
 
 module.exports = {
-  SESSION_ID: "Jarvis_85z6_5483_04z1_1wvu_z06v_5zv9_68y0_16x3", 
+  SESSION_ID: "", 
   
   GEMINI_API_KEY: "AIzaSyDDPoDW5-4wOz2xUPtX0LF2HXvTUxN8VAY", 
   NEWS_API_KEY: "53a3e0d4f6bb42ea964e30ba3fd3fc7f", // Get from newsapi.org
