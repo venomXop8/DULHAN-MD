6a9f330f0e7b2ac0a3f03496e90506ac1883f5f1
IRONMAN x VENOM <gautamdipanshu475@gmail.com>
2025-09-22 01:22:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 // DULHAN-MD Configuration File
 
 module.exports = {
-  SESSION_ID: "", 
+  SESSION_ID: "Jarvis_85z6_5483_04z1_1wvu_z06v_5zv9_68y0_16x3", 
   
   GEMINI_API_KEY: "AIzaSyDDPoDW5-4wOz2xUPtX0LF2HXvTUxN8VAY", 
-  NEWS_API_KEY: "YOUR_NEWSAPI.ORG_KEY_HERE", // Get from newsapi.org
-  CURRENCY_API_KEY: "YOUR_CURRENCYAPI.COM_KEY_HERE", // Get from currencyapi.com
+  NEWS_API_KEY: "53a3e0d4f6bb42ea964e30ba3fd3fc7f", // Get from newsapi.org
+  CURRENCY_API_KEY: "cur_live_8CI5A3UVLuOcYvdPQxgrxJiIzpAx6GZ505OHTThA", // Get from currencyapi.com
   WEATHER_API_KEY: "532ec7db4edf41a516790b17210be621",
   REMOVEBG_API_KEY: "", 
   BOT_NAME: "DULHAN-MD",
-  OWNER_NAME: "Malik Sahab",
-  OWNER_NUMBER: "923322964709",
+  OWNER_NAME: "Ironman",
+  OWNER_NUMBER: "919719313814",
   CHANNEL_LINK: "https://whatsapp.com/channel/0029VaN8WMOHFxP0SLAKKu0P",
   AUDIO_REPLY_PATH: "./dulhan_audio.mp3",
   PROFILE_PIC_URL: "https://files.catbox.moe/wz96cv.jpg",
